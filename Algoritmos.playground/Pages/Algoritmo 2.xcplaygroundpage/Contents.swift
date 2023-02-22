0d764fe3de11cabe44e0e4f254c703e9c3536784
Luciano Castro <lfcastro2911@gmail.com>
2023-02-22 17:39:16 -0300

    agregado segundo algoritmo

--- Algoritmos.playground/Pages/Algoritmo 2.xcplaygroundpage/Contents.swift
+++ Algoritmos.playground/Pages/Algoritmo 2.xcplaygroundpage/Contents.swift
@@ -2,6 +2,37 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+// Definimos un arreglo de números ordenados de forma ascendente.
+let numbers = [1,2,3,4,5,6,7,8,20]
 
-//: [Next](@next)
+// Definimos una función de búsqueda binaria que recibe un arreglo de enteros y un valor entero a buscar. Retorna un valor booleano.
+func binarySearch( array: [Int], value: Int) -> Bool {
+    // Definimos dos variables enteras para indicar el índice de la posición izquierda y derecha en el arreglo.
+    var left = 0
+    var right = array.count - 1
+
+    // Ejecutamos un bucle while mientras el índice izquierdo sea menor o igual que el derecho.
+    while left <= right {
+        // Calculamos el índice del medio del arreglo.
+        let middle = (left + right) / 2
+
+        // Si el número en la posición del medio es igual al valor que buscamos, retornamos verdadero.
+        if array[middle] == value {
+            return true
+        }
+        // Si el número en la posición del medio es menor que el valor que buscamos, descartamos la mitad izquierda del arreglo.
+        else if array[middle] < value {
+            left = middle + 1
+        }
+        // Si el número en la posición del medio es mayor que el valor que buscamos, descartamos la mitad derecha del arreglo.
+        else {
+            right = middle - 1
+        }
+    }
+
+    // Si el valor no se encuentra en el arreglo, retornamos falso.
+    return false
+}
+
+// Llamamos a la función de búsqueda binaria y le pasamos el arreglo y el valor a buscar. En este caso, buscamos el valor 4 en el arreglo.
+binarySearch(array: numbers, value: 4)
