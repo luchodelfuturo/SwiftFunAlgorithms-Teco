babac64277cdd8bc3da88cb64c0cae3324b97a00
Luciano Castro <lfcastro2911@gmail.com>
2023-02-23 11:16:59 -0300

    Agregado Algoritmo 3

--- Algoritmos.playground/Pages/Algoritmo 3.xcplaygroundpage/Contents.swift
+++ Algoritmos.playground/Pages/Algoritmo 3.xcplaygroundpage/Contents.swift
@@ -2,6 +2,19 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//Recursividad y Factorial
 
-//: [Next](@next)
+// Esta función calcula el factorial de un número de forma recursiva
+func factorialRecursivo(value: Int) -> Int{
+    
+    // Si el valor proporcionado es 0, el factorial es 1
+    if value == 0 {
+        return 1
+    }
+    
+    // De lo contrario, se llama a la función de forma recursiva con el valor anterior al proporcionado
+    return value * factorialRecursivo(value: value - 1)
+}
+
+// Se llama a la función con el valor 3
+factorialRecursivo(value: 3)
