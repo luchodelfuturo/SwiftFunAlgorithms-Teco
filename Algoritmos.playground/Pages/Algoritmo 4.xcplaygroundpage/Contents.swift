67f64cebabde3aa804255cd82030e911cda490aa
Luciano Castro <lfcastro2911@gmail.com>
2023-02-23 11:48:23 -0300

    agregado Algoritmo 4

--- Algoritmos.playground/Pages/Algoritmo 4.xcplaygroundpage/Contents.swift
+++ Algoritmos.playground/Pages/Algoritmo 4.xcplaygroundpage/Contents.swift
@@ -2,6 +2,31 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+func encontrarElementoMasFrecuente(in array: [String]) -> String? {
+    var frecuencias = [String: Int]() // Crear un diccionario para contar la frecuencia de cada elemento
+    var elementoMasFrecuente: String?
+    var frecuenciaMasAlta = 0
+    
+    // Iterar sobre el array y actualizar las frecuencias
+    for elemento in array {
+        if let frecuencia = frecuencias[elemento] {
+            frecuencias[elemento] = frecuencia + 1
+        } else {
+            frecuencias[elemento] = 1
+        }
+        
+        // Actualizar el elemento más frecuente si es necesario
+        if let frecuencia = frecuencias[elemento], frecuencia > frecuenciaMasAlta {
+            frecuenciaMasAlta = frecuencia
+            elementoMasFrecuente = elemento
+        }
+    }
+    
+    return elementoMasFrecuente
+}
+let nombres = ["Ana", "Juan", "María", "Pedro", "Luisa", "Juan", "María", "Luisa", "Juan"]
+if let elementoMasFrecuente = encontrarElementoMasFrecuente(in: nombres) {
+    print("El elemento más frecuente es \(elementoMasFrecuente)")
+} else {
+    print("El array está vacío")
+}
